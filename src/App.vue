8f5b02628fb88947cfb6d85228560948e0220443
Selena Siri <selenasiri@gmail.com>
2022-11-11 15:28:25 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1>Vue Lists and Conditional Content</h1>
+  </header>
+  <section id="app">
+    <h2>Assignment</h2>
+    <!-- 1) Add code to manage a list of tasks in a Vue app -->
+    <!-- When clicking "Add Task" a new task with the entered text should be added -->
+    <input type="text" v-model="enteredTaskValue" />
+    <button @click="addTask">Add Task</button>
+    <!-- 2) Output the list of tasks here -->
+    <ul v-if="listIsVisible">
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+    <!-- 3) When the below button is pressed, the list should be shown or hidden -->
+    <!-- BONUS: Also update the button caption -->
+    <button @click="toggleTaskList">
+      <h1>{{ buttonCaption }}</h1>
+    </button>
+    <!-- if goals = 0 "Add Tasks" -->
+    <!-- if goals > 0, "Hide list" -->
+    <!-- else "Show list" -->
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      enteredTaskValue: '',
+      tasks: [],
+      listIsVisible: true,
+    };
+  },
+  computed: {
+    buttonCaption() {
+      return this.listIsVisible ? 'Hide List' : 'Show List';
+    },
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.enteredTaskValue);
+      this.enteredTaskValue = '';
+    },
+    removeTask(idx) {
+      this.tasks.splice(idx, 1);
+    },
+    toggleTaskList() {
+      this.listIsVisible = !this.listIsVisible;
+    },
+  },
+};
 </script>
 
 <style>
